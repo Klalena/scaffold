e81bcea40fc49651194b7e9cab0e045e2ecf2a91
Alena Kalodzitsa <alena.kalodzitsa@gmail.com>
2021-01-30 22:41:57 +0000

    adding initial structure

---
+++ hello.py
@@ -0,0 +1,10 @@
+"""This module has an add function that adds two inputs. """
+
+
+def add(vab_x, var_y):
+    """This function adds x and y and return their sum."""
+    return vab_x + var_y
+
+
+RESULT = add(3, 2)
+print(f"This is the sum of 3 and 2, which is {RESULT}")
